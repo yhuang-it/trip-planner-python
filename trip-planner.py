68e6236218200f26b19c79119b94bb3f3f5952f3
yhuang-it <107271362+yhuang-it@users.noreply.github.com>
2022-08-21 12:32:38 -0400

    Create trip-planner.py

---
+++ trip-planner.py
@@ -0,0 +1,18 @@
+def trip_planner_welcome(name):
+  print("Welcome to tripplanner v1.0 " + name)
+
+trip_planner_welcome("Yashi")
+
+def estimated_time_rounded(estimated_time):
+  rounded_time = round(estimated_time)
+  return rounded_time
+
+estimate = estimated_time_rounded(1.02)
+
+def destination_setup(origin, destination, estimated_time, mode_of_transport="Car"):
+  print("Your trip starts off in " + origin)
+  print("And you are traveling to " + destination)
+  print("You will be traveling by " + mode_of_transport)
+  print("It will take approximately " + str(estimated_time) + " hours")
+
+destination_setup("Time Square", "Little Italy", estimate, "bus")
